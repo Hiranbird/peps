48a21fabb578650619efca5abaf80940fcbc25c6
Hiranbird <99292142+Hiranbird@users.noreply.github.com>
2023-01-01 17:14:59 -0500

    Add files via upload

---
+++ assets/preload/stages/pepa.lua
@@ -0,0 +1,21 @@
+function onCreate()
+	-- background shit
+	makeLuaSprite('stage1', 'stage1', -600, -300);
+	setScrollFactor('stage1', 0.9, 0.9);
+	
+	addLuaSprite('stage1', false);
+	
+	makeLuaSprite('tran','tran',09,0)
+ makeAnimatedLuaSprite('tran', 'tran', -09,0)
+	addAnimationByPrefix('tran', 'tran', 'tran', 24, true)
+        setScrollFactor('tran', 0, 0);
+        setObjectCamera('tran', 'other');
+        scaleObject('tran', 0.999, 0.999);
+        addLuaSprite('tran', true);
+
+makeLuaSprite('blackvignette','blackvignette',-31,0)
+        setScrollFactor('blackvignette', 0, 0);
+        setObjectCamera('blackvignette', 'other');
+        scaleObject('blackvignette', 0.999, 0.999);
+        addLuaSprite('blackvignette', true);
+end
